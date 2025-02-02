edac57a6074883bad4d9e4fdb70d192190b087fc
loki <loyde.b.bjerke@gmail.com>
2025-02-02 14:51:58 -0600

    updtea;2;13

--- nix/mkNeovim.nix
+++ nix/mkNeovim.nix
@@ -13,14 +13,14 @@ with lib;
     appName ? null,
     # The Neovim package to wrap
     neovim-unwrapped ? pkgs-wrapNeovim.neovim-unwrapped,
-    plugins ? [ {name = "music-controls"; url = "https://github.com/AntonVanAssche/music-controls.nvim"; defaultPlugin = {runtime = {playerctl = pkgs.playerctl;};};} ], # List of plugins
+    plugins ? [  ], # List of plugins
     # List of dev plugins (will be bootstrapped) - useful for plugin developers
     # { name = <plugin-name>; url = <git-url>; }
     devPlugins ? [],
     # Regexes for config files to ignore, relative to the nvim directory.
     # e.g. [ "^plugin/neogit.lua" "^ftplugin/.*.lua" ]
    ignoreConfigRegexes ? [],
-    extraPackages ? p: [p.playerctl], # Extra runtime dependencies (e.g. ripgrep, ...)
+    extraPackages ? p: [], # Extra runtime dependencies (e.g. ripgrep, ...)
     # The below arguments can typically be left as their defaults
     # Additional lua packages (not plugins), e.g. from luarocks.org.
     # e.g. p: [p.jsregexp]
@@ -50,7 +50,7 @@ with lib;
       runtime = {};
     };
 
-    externalPackages = extraPackages ++ (optionals withSqlite [pkgs.sqlite]);
+    externalPackages = extraPackages ++ (optionals withSqlite [pkgs.sqlite pkgs.playerctl]);
 
     # Map all plugins to an attrset { plugin = <plugin>; config = <config>; optional = <tf>; ... }
     normalizedPlugins = map (x:
