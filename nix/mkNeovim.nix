8f319afad2aacfaa7e43531ffef136aececf33a8
loki <loyde.b.bjerke@gmail.com>
2025-02-02 13:50:52 -0600

    update:

--- nix/mkNeovim.nix
+++ nix/mkNeovim.nix
@@ -20,7 +20,7 @@ with lib;
     # Regexes for config files to ignore, relative to the nvim directory.
     # e.g. [ "^plugin/neogit.lua" "^ftplugin/.*.lua" ]
    ignoreConfigRegexes ? [],
-    extraPackages ? [playerctl], # Extra runtime dependencies (e.g. ripgrep, ...)
+    extraPackages ? [pkgs.playerctl], # Extra runtime dependencies (e.g. ripgrep, ...)
     # The below arguments can typically be left as their defaults
     # Additional lua packages (not plugins), e.g. from luarocks.org.
     # e.g. p: [p.jsregexp]
