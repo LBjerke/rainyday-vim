1f9fc81c60eca790c625907b3812b7d088961a75
loki <loyde.b.bjerke@gmail.com>
2025-02-02 12:10:26 -0600

    update

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -27,12 +27,51 @@ local function extra_mode_status()
   return ''
 end
 require("pomo").setup({
+  -- How often the notifiers are updated.
+  update_interval = 1000,
+
+  -- Configure the default notifiers to use for each timer.
+  -- You can also configure different notifiers for timers given specific names, see
+  -- the 'timers' field below.
   notifiers = {
+    -- The "Default" notifier uses 'vim.notify' and works best when you have 'nvim-notify' installed.
     {
-      ops = { sticky = false },
+      name = "Default",
+      opts = {
+        -- With 'nvim-notify', when 'sticky = true' you'll have a live timer pop-up
+        -- continuously displayed. If you only want a pop-up notification when the timer starts
+        -- and finishes, set this to false.
+        sticky = false,
+
+        -- Configure the display icons:
+        title_icon = "󱎫",
+        text_icon = "󰄉",
+        -- Replace the above with these if you don't have a patched font:
+        -- title_icon = "⏳",
+        -- text_icon = "⏱️",
+      },
+    },
+
+    -- The "System" notifier sends a system notification when the timer is finished.
+    -- Available on MacOS and Windows natively and on Linux via the `libnotify-bin` package.
+    { name = "System" },
+
+    -- You can also define custom notifiers by providing an "init" function instead of a name.
+    -- See "Defining custom notifiers" below for an example 👇
+    -- { init = function(timer) ... end }
+  },
+
+  -- Override the notifiers for specific timer names.
+  timers = {
+    -- For example, use only the "System" notifier when you create a timer called "Break",
+    -- e.g. ':TimerStart 2m Break'.
+    Break = {
+      { name = "System" },
     },
   },
+  -- You can optionally define custom timer sessions.
   sessions = {
+    -- Example session configuration for a session called "pomodoro".
     pomodoro = {
       { name = "Work",        duration = "25m" },
       { name = "Short Break", duration = "5m" },
@@ -88,7 +127,11 @@ require('lualine').setup {
   --   lualine_c = {},
   lualine_x = {
     function()
-      local pomo = require("pomo")
+      local ok, pomo = pcall(require, "pomo")
+      if not ok then
+        print(ok)
+        return ""
+      end
 
       local timer = pomo.get_first_to_finish()
       if timer == nil then
