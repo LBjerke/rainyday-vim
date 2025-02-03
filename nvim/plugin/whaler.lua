e27ee6c1b31a0e621fbe79249e5ecea222f34c43
loki <loyde.b.bjerke@gmail.com>
2025-02-03 17:56:22 -0600

    configuration of whaler

---
+++ nvim/plugin/whaler.lua
@@ -0,0 +1,21 @@
+-- Telescope setup()
+local telescope = require('telescope')
+
+telescope.setup({
+    -- Your telescope setup here...
+    extensions = {
+        whaler = {
+            -- Whaler configuration
+            directories = { "~/Code", { path = "/etc/nix/", alias = "nix" } },
+            -- You may also add directories that will not be searched for subdirectories
+            -- oneoff_directories = { "path/to/project/folder",  { path = "path/to/another/project", alias = "Project Z" } },
+        }
+    }
+})
+-- More config here
+telescope.load_extension("whaler")
+--
+
+
+-- Or directly
+vim.keymap.set("n", "<leader>fw", telescope.extensions.whaler.whaler)
