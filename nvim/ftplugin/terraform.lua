61f982d86fdfd9a6ba2273427e0130988dae6582
Loyde Cochran-Bjerke <Loyde.b.bjerke@gmail.com>
2025-03-25 11:12:53 -0500

    Update terraform.lua

--- nvim/ftplugin/terraform.lua
+++ nvim/ftplugin/terraform.lua
@@ -1,1 +1,19 @@
 
+if vim.fn.executable('terraform-ls') ~= 1 then
+  return
+end
+
+local root_files = {
+  "main.tf",
+  '.git',
+}
+
+vim.lsp.start {
+  name = 'terraform-ls',
+  cmd = { "terraform-ls" , "serve" },
+  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  capabilities = require('user.lsp').make_client_capabilities(),
+  filetypes = { "terraform", "terraform-vars" },
+  single_file_support = true,
+
+}
